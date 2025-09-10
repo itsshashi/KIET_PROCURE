33758d94b5a72bedc0ed11c974dc97c223d6c68f
Shashank N <shashankn155555@gmail.com>
2025-09-10 16:18:35 +0530

    commit changes

--- print.js
+++ print.js
@@ -106,7 +106,7 @@ function generatePurchaseOrder(poData, filePath) {
 
   const cgst = subtotal * 0.09;
   const sgst = subtotal * 0.09;
-  const grandTotal = subtotal + cgst + sgst;
+  const grandTotal = subtotal;
 
   // 👉 Document definition
   const docDefinition = {
@@ -202,16 +202,16 @@ function generatePurchaseOrder(poData, filePath) {
     widths: ["*", "auto"],
     body: [
       
-      ["Subtotal", { text: subtotal.toFixed(2), alignment: "right" ,margin:[0,0,0,0] }],
-      ["CGST @ 9%", { text: cgst.toFixed(2), alignment: "right" ,margin:[0,0,0,0]}],
-      ["SGST @ 9%", { text: sgst.toFixed(2) , alignment: "right",margin:[80,0,0,0]}],
+      [],
+      [{text:"**include with Gst**", alignment: "right" ,margin:[0,0,0,0]}],
+      
       [
         { text: "Grand Total", bold: true },
         { text: grandTotal.toFixed(2), bold: true, alignment: "right" },
       ],
     ],
   },
-  layout: horizontalLineLayout,
+  layout:horizontalLineLayout,
   margin: [0, 10, 0, 15],
   font:"Times"
 },
@@ -245,33 +245,26 @@ function generatePurchaseOrder(poData, filePath) {
 {
   ol: [
     { text: 'Acceptance / Modification: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' , 
-      stack: ['Orders are binding only upon written confirmation. Any modifications require mutual agreement.'] },
+      stack: ['Orders are binding only upon written confirmation. Any modifications require mutual agreement./within 48 hrs'] },
     { text: 'Delivery, Shipment & Packaging: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Supplier must ensure timely delivery, safe shipment, and proper packaging to avoid damages.'] },
-    { text: 'Excusable Delay: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Delays due to force majeure (natural disasters, strikes, etc.) may be excused if communicated promptly.'] },
-    { text: 'Delivery Terms / Risk of Loss: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Risk transfers to the buyer only after goods are received in good condition at the agreed location.'] },
+    
+    
     { text: 'Import / Customs Compliance: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Supplier must comply with applicable import/export and customs regulations.'] },
     { text: 'Price: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Prices are firm and inclusive of all applicable duties, taxes, and charges unless agreed otherwise.'] },
     { text: 'Invoicing & Payment: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Invoices must match the purchase order details. Payments will follow agreed terms.'] },
+      stack: ['Invoices must match the purchase order details. Non conforming goods may be rejected'] },
     { text: 'Inspection: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['All goods are subject to inspection and approval upon delivery. Non-conforming goods may be rejected.'] },
-    { text: 'Warranty: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Supplier warrants that goods are free from defects in material, design, and workmanship.'] },
+      stack: ['All goods are subject to inspection and approval upon delivery. Payment will follow agreed terms'] },
+    
     { text: 'Changes: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Buyer reserves the right to request reasonable changes in scope, specifications, or delivery.'] },
     { text: 'Design & Process Changes: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Any design or process modifications by supplier require prior written approval.'] },
-    { text: 'Termination: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['The buyer may terminate the order for default, insolvency, or breach of contract terms.'] },
-    { text: 'General Indemnification: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Supplier shall indemnify the buyer against any claims arising from negligence or misconduct.'] },
-    { text: 'Intellectual Property Indemnification: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Supplier guarantees that goods do not infringe on any third-party intellectual property rights.'] },
+    
+    
     { text: 'Insurance: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Supplier must maintain adequate insurance coverage for goods and liabilities.'] },
     { text: 'Confidentiality: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
@@ -279,18 +272,15 @@ function generatePurchaseOrder(poData, filePath) {
     { text: 'Audit: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['Buyer reserves the right to audit supplier’s compliance with contractual obligations.'] },
     { text: 'Compliance with Laws & Integrity: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
-      stack: ['Supplier must comply with all applicable laws, regulations, and ethical standards.'] },
+      stack: ['Supplier must comply with all Quality inspection Standards, regulations, and ethical standards.'] },
     { text: 'Applicable Law & Forum: ', bold: false, margin: [0, 0, 0, 5], alignment: 'justify' ,
       stack: ['This contract shall be governed by applicable law, and disputes settled under the agreed jurisdiction.'] },
-      { text: 'Compliance with Laws: The supplier shall comply with all applicable laws, export regulations, and ethical business practices at all times. Any form of bribery, gratification, or involvement of restricted materials is strictly prohibited.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-    { text: 'Material Restrictions: The goods supplied must not contain iron or steel originating from sanctioned countries.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
+     
     { text: 'Invoicing: All invoices must exactly match the purchase order details, clearly reference the PO number, and be submitted within three days of issuance.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-    { text: 'Payment Terms: Payments will be made within forty-five days from the date of goods receipt or invoice receipt, whichever is applicable.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
+    { text: 'Payment Terms: Payments will be made within date mentioned in the Quotation from the date of goods receipt or invoice receipt, whichever is applicable.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
     { text: 'Delivery Timing & Routing: Deliveries will only be accepted from Monday to Friday between 9:00 AM and 5:00 PM, and must be routed through the designated material gates.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-    { text: 'Delivery Documentation: Each delivery must be accompanied by three copies of the invoice to ensure proper processing.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-    { text: 'Personnel Requirements: Supplier personnel entering the premises must wear safety shoes and carry valid identification, driving licenses, and vehicle documents.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-    { text: 'Right of Rejection / Termination: The buyer reserves the right to reject goods or terminate the purchase order immediately in the event of non-compliance with these conditions.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
-  ],
+    { text: 'Delivery Documentation: Each delivery must be accompanied by three copies of the invoice to ensure proper processing and Quality Check Complete Report.', bold: false, alignment: 'justify', margin: [0, 0, 0, 5] },
+    ],
   font: 'Roboto',
   fontSize: 10,
   lineHeight: 1.3
