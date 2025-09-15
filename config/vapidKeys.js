afa0a3a8bbda3259c5a88bc321dfd6f987e6f978
Shashank N <shashankn155555@gmail.com>
2025-09-16 00:15:51 +0530

    complete changes

---
+++ config/vapidKeys.js
@@ -0,0 +1,6 @@
+const vapidKeys = {
+  publicKey: 'BLYAv-lfOcJyOEZWl6BZCtdiGSAwDHgdmM-8R52OsdUm2QdaWaP0TPKVEr688gJbGbs0dAOSNf3tNuJeZAnoe4k',
+  privateKey: '2_S4Y3kZdp0yqmthIJK0_cgvSPMvPD65ZhIITVnw3iA'
+};
+
+export default vapidKeys;
