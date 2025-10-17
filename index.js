6d07d4af456563feb60e8e4508c99bacee04e464
Shashank N <shashankn155555@gmail.com>
2025-10-17 18:03:06 +0530

    change the cookie setting of user

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(session({
     secret: 'super-secret-key',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // 24 hours
 }));
 app.use(express.urlencoded({ extended: true ,limit: '50mb'}));
 
