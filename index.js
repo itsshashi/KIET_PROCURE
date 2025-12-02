30a0016029d245f2c17d41ea820e5d28f8ae6978
Shashank N <shashankn155555@gmail.com>
2025-12-02 23:33:35 +0530

    co

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const pool = new Pool({
   user: "postgres",
   host: "13.234.3.0",
   database: "mydb",
-  password:db_pass,
+  password:"Shashank@KIET1519",
   port: 5432,
 });
 app.use('/qt_uploads', express.static(path.join(__dirname, 'qt_uploads')));
