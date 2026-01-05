52294c09d82e1c2fa0069a183bae362130f99db4
Shashank N <shashin1504@gmail.com>
2026-01-05 15:00:54 +0530

    commit changes

--- index.js
+++ index.js
@@ -7782,5 +7782,8 @@ app.get("/api/history", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000; // use Render's PORT if available
-app.listen(PORT, () => console.log(`🚀 Server running on port! ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
