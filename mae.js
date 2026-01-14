d567aaff008db7030ff074096641591ddfae22b9
Shashank N <shashin1504@gmail.com>
2026-01-14 09:30:16 +0530

    commit changes

--- mae.js
+++ mae.js
@@ -561,8 +561,8 @@ const maeContent = buildMaeContent(poData.textareaDetails);
 
     { text: "Respected Sir,", bold: true, margin: [0, 0, 0, 8] },
     { text: `Kind Attention: ${poData.requester.name || ""}`, bold: true, margin: [0, 0, 0, 12] },
-
-    { text: `Subject: Quotation for Design, Development, Manufacturing, Supply, and Installation of  ${poData.machine || ""}`, bold: true, decoration: "underline", margin: [0, 0, 0, 6] },
+//Design, Development, Manufacturing, Supply, and Installation of 
+    { text: `Subject: Quotation for  ${poData.machine || ""}`, bold: true, decoration: "underline", margin: [0, 0, 0, 6] },
     { text: `Ref: ${poData.reference || "Email"}`, italics: true, margin: [0, 0, 0, 14] },
 
 {
